ddc28bdff7290c42d76c2b774becfeb0a520e922
Ivancito-ur <61300455+Ivancito-ur@users.noreply.github.com>
2020-05-07 08:26:16 -0500

    Quinta media hora

---
+++ ajax.js
@@ -0,0 +1,41 @@
+
+function registro() {
+    var peticion = null;
+
+    peticion = new XMLHttpRequest();
+    console.log(peticion);
+    if (peticion) {
+        peticion.open('get', "registro.html", false); //metodo , el archivo que voy a traer , si lo quiero sincrono = falso
+        peticion.send(null);
+        document.getElementById("contenedor").innerHTML = peticion.responseText;
+
+    }
+}
+function producto_1() {
+    var peticion = null;
+
+    peticion = new XMLHttpRequest();
+    console.log(peticion);
+    if (peticion) {
+        peticion.open('get', "producto_1.html", false); //metodo , el archivo que voy a traer , si lo quiero sincrono = falso
+        peticion.send(null);
+        document.getElementById("contenedor_menor").innerHTML = peticion.responseText;
+
+    }
+}
+
+
+function contacto() {
+
+    var peticion = null;
+
+    peticion = new XMLHttpRequest();
+    console.log(peticion);
+    if (peticion) {
+        peticion.open('get', "contacto.html", false); //metodo , el archivo que voy a traer , si lo quiero sincrono = falso
+        peticion.send(null);
+        document.getElementById("contenedor").innerHTML = peticion.responseText;
+
+    }
+    
+}
